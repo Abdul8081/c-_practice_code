9abbd75e53016e2ec4a66f51538e304d5fba0b61
Abdul8081 <amuid677@gmail.com>
2023-12-30 21:40:48 +0530

    basics_calculation_added
    This is just a basic code

---
+++ Initial/basicsCalculation.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<"addition = "<<a+b<<endl;
+    cout<<"subtraction = "<<a-b<<endl;
+    cout<<"multiplication = "<<a*b<<endl;
+    cout<<"division = "<<a/b<<endl;
+    cout<<"modulus = "<<a%b<<endl;
+    
+}
