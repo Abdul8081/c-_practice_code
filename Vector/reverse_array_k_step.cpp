c02df6ca62ecb4a0a4526102dca0b1b71c8d5ef9
Abdul8081 <amuid677@gmail.com>
2024-01-02 14:00:55 +0530

    rotate array by k step is added

---
+++ Vector/reverse_array_k_step.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+void reversePart(vector<int> &v, int i, int j){
+    int n = v.size();
+    while(i<=j){
+        int temp = v[i];
+        v[i] = v[j];
+        v[j] = temp;
+        i++;
+        j--;
+    }
+    return;
+}
+void rotate_by_kstep(vector<int> &v, int k){
+    int n = v.size();
+    reversePart(v,0,n-1-k);
+    reversePart(v,n-k,n-1);
+    reversePart(v,0,n-1);
+
+}
+int main(){
+    int n; cin>>n;
+    vector<int> v;
+    for(int i=0;i<n;i++){
+        int x; cin>>x;
+        v.push_back(x);
+    }
+    int k; cin>>k;
+    rotate_by_kstep(v,k);
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
